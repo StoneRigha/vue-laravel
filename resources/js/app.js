24871f7be27fc609d0d64b1477223d320931c275
StoneRiga <stoneriga@gmail.com>
2021-11-30 15:28:56 +0300

    import of routers, const of routers, components updatw

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 require('./bootstrap');
 window.Vue = require('vue');
 
-
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
 
 import App from './App.vue';
+import Home from '../js/components/Home';
+import ContactsList from '../js/components/ContactsList';
 
 
 
@@ -11,4 +14,17 @@ import VueAxios from 'vue-axios';
 import axios from 'axios';
 Vue.use(VueAxios, axios);
 
+const routes = [
+    {
+        name: '/',
+        path: '/',
+        component: Home
+    },
+    {
+        name: '/contacts',
+        path: '/contacts',
+        component: ContactsList
+    }
+]
+const router = new VueRouter({routes: routes});
 const app = new Vue(vue.util.extend(App)).$mount('#app');
