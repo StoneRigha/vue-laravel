a8d98d7ab36c64a362f9cc058400b58287157107
StoneRiga <stoneriga@gmail.com>
2021-11-30 13:04:23 +0300

    update B.S CDN, CSRF token Import of vue in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 require('./bootstrap');
+window.Vue = require('vue');
+
+
+
+import App from './App.vue';
+
+
+
+import VueAxios from 'vue-axios';
+import axios from 'axios';
+Vue.use(VueAxios, axios);
+
+const app = new Vue(vue.util.extend(App)).$mount('#app');
